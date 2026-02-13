193d8f90357639ef44499057f5c69160e9c37972
sangeethvdk <134766863+sangeethvdk@users.noreply.github.com>
2026-02-13 23:07:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-let musicPlayed=false;
-let currentImageIndex=0;
+let musicPlayed = false;
+let currentImageIndex = 0;
 
-const images=[
+const images = [
 "img1.jpg","img2.jpg","img3.jpg","img4.jpg","img5.jpg",
 "img6.jpg","img7.jpg","img8.jpg","img9.jpg","img10.jpg"
 ];
 
-const captions=[
-"Memory 1 ❤️","Memory 2 💕","Memory 3 ✨","Memory 4 🤍",
-"Memory 5 💖","Memory 6 🎶","Memory 7 🌸",
-"Memory 8 🚗","Memory 9 💍","Memory 10 ❤️"
+const captions = [
+"College Bunk kotti Ninnu PickUp Chesukovadam❤️",
+"Infosys lo Faculty mudhe Photoshoot🤭",
+"Class Lo Photoshoot✨",
+"Mana 1st Bike Ride Day🤍",
+"Mana Last Working Day In Diploma😒",
+"Substation Ki Malli Podhama🫣",
+"Travalled 400+ Kms to See You🌸",
+"Hyderabad Lo Mana 1st Outing🥰",
+"TankBund With Rain & You🫶",
+"My Moon Day🖤"
 ];
 
 function goToPage(num){
@@ -20,10 +27,12 @@ if(num===2 && !musicPlayed){
 let music=document.getElementById("bgMusic");
 music.volume=0;
 music.play();
+
 let fade=setInterval(()=>{
 if(music.volume<1) music.volume+=0.05;
 else clearInterval(fade);
 },200);
+
 musicPlayed=true;
 }
 
@@ -34,8 +43,13 @@ document.getElementById("finalReveal").classList.add("show");
 }
 }
 
-function playVideo(){ document.getElementById("loveVideo").play(); }
-function pauseVideo(){ document.getElementById("loveVideo").pause(); }
+function playVideo(){
+document.getElementById("loveVideo").play();
+}
+
+function pauseVideo(){
+document.getElementById("loveVideo").pause();
+}
 
 function openImage(index){
 currentImageIndex=index;
@@ -46,9 +60,15 @@ document.getElementById("modalCaption").innerText=captions[index];
 
 function changeImage(direction){
 currentImageIndex+=direction;
-if(currentImageIndex<0) currentImageIndex=images.length-1;
-if(currentImageIndex>=images.length) currentImageIndex=0;
-openImage(currentImageIndex);
+
+if(currentImageIndex<0)
+currentImageIndex=images.length-1;
+
+if(currentImageIndex>=images.length)
+currentImageIndex=0;
+
+document.getElementById("modalImage").src=images[currentImageIndex];
+document.getElementById("modalCaption").innerText=captions[currentImageIndex];
 }
 
 function closeImage(){
@@ -56,12 +76,16 @@ document.getElementById("imageModal").style.display="none";
 }
 
 const startDate=new Date("2022-09-13");
+
 setInterval(()=>{
 const now=new Date();
 const diff=now-startDate;
+
 const days=Math.floor(diff/(1000*60*60*24));
 const hours=Math.floor((diff/(1000*60*60))%24);
 const minutes=Math.floor((diff/(1000*60))%60);
+
 document.getElementById("loveCounter").innerText=
 days+" Days "+hours+" Hours "+minutes+" Minutes ❤️";
+
 },1000);
